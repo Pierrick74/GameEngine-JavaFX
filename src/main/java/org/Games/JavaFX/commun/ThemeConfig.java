a20284381b5bae747cae80331f731eac2608aa0a
pierrick <pierrickviret@gmail.com>
2025-10-20 14:25:35 +0200

    change gitignore

--- src/main/java/org/Games/JavaFX/commun/ThemeConfig.java
+++ src/main/java/org/Games/JavaFX/commun/ThemeConfig.java
@@ -5,7 +5,6 @@ import javafx.scene.layout.Region;
 
 public class ThemeConfig {
     // Couleurs
-    public static final String TEXT_LIGHT = "#264653";
     public static final String TEXT = "#e8e8e8";
     public static final String BUTTON_YELLOW = "#06b6d4";
 
@@ -13,12 +12,6 @@ public class ThemeConfig {
     public static final String DARK_BACKGROUND_STYLE =
             "-fx-background-color: #0a1428;" +
                     "-fx-alignment: center; ";
-
-    public static final String PLAYER_INFORMATION_STYLE =
-            "-fx-background-color: #8B7355;" +
-                    "-fx-background-radius: 10;" +
-                    "-fx-alignment: center;" +
-                    "-fx-effect: dropshadow(three-pass-box, #000000, 10, 0, 5, 5);";
 
     public static final String BUTTON_STYLE =
             "-fx-background-color: " + BUTTON_YELLOW + ";" +
@@ -28,23 +21,14 @@ public class ThemeConfig {
                     "-fx-border-color: " + TEXT + ";" +
                     "-fx-border-width: 2;";
 
-    public static final String INPUT_STYLE =
-            "-fx-border-color: " + TEXT + ";" +
-                    "-fx-border-width: 2;";
-
     // Méthodes utilitaires
     public static void applyDarkBackground(Region node) {
         node.setStyle(DARK_BACKGROUND_STYLE);
     }
 
-    public static void applyPlayerTheme(Region node) {
-        node.setStyle(PLAYER_INFORMATION_STYLE);
-    }
-
     public static void applyButtonStyle(Button button) {
         String fullStyle = BUTTON_STYLE +
                 "-fx-background-insets: 0;" +
-                "-fx-background-radius: 8;" +
                 "-fx-border-radius: 8;";
 
         button.setStyle(fullStyle);
